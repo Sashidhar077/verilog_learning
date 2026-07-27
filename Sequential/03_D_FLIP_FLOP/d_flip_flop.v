50200e488aec2dabce2dd2376688e224d297d425
Sashidhar077 <sahasra972@gmail.com>
2026-07-27 10:29:11 +0000

    D FLIP FLOP

--- Sequential/03_D_FLIP_FLOP/d_flip_flop.v
+++ Sequential/03_D_FLIP_FLOP/d_flip_flop.v
@@ -1,7 +1,6 @@
-//Asynchronous D flip flop 
-
 module d_flip_flop(
-        input D,rst,clk,
+        input D,clk,
+        input rst, //Asynchronous active high reset
         output reg Q,Qbar
 );
 
