d2f564948a665ca62680dc7577d1ae95dc1d1a62
Sashidhar077 <sahasra972@gmail.com>
2026-07-29 08:17:08 +0000

    jk flip flop

--- Sequential/04_JK_FLIP_FLOP/jk_flip_flop.v
+++ Sequential/04_JK_FLIP_FLOP/jk_flip_flop.v
@@ -8,14 +8,14 @@ assign Qbar = ~Q;
 
 always@(posedge clk) begin 
     if(rst) begin 
-        Q=1'b0;
+        Q<=1'b0;
     end
     else begin 
         case ({J,K})
-            2'b00:Q=Q;
-            2'b01:Q=1'b0;
-            2'b10:Q=1'b1;
-            2'b11:Q=~Q;
+            2'b00:Q<=Q;
+            2'b01:Q<=1'b0;
+            2'b10:Q<=1'b1;
+            2'b11:Q<=~Q;
         endcase
     end
 end
